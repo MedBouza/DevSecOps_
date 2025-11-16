3f0990e98debafbb4da56fc772aec30f2d84dad3
Mohamed BOUZAYANI <147999857+MohamedBouzayani@users.noreply.github.com>
2025-11-16 06:51:17 +0100

    test_webhook7

--- src/test/java/tn/esprit/spring/SubscriptionServiceTest.java
+++ src/test/java/tn/esprit/spring/SubscriptionServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.spring.entities.Skier;
 import tn.esprit.spring.entities.Subscription;
@@ -25,6 +26,7 @@ import static org.mockito.Mockito.when;
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class SubscriptionServiceTest {
     @Autowired
     private SubscriptionServicesImpl subscriptionService;
