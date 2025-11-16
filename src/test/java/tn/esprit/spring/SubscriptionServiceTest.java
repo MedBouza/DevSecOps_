8f048711dbfaa5a30d09f93e4b15917385c0206e
Mohamed BOUZAYANI <147999857+MohamedBouzayani@users.noreply.github.com>
2025-11-16 08:10:35 +0100

    Fix Mockito dependency issue - add Byte Buddy

--- src/test/java/tn/esprit/spring/SubscriptionServiceTest.java
+++ src/test/java/tn/esprit/spring/SubscriptionServiceTest.java
@@ -31,6 +31,15 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@org.springframework.test.context.TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.defer-datasource-initialization=false",
+    "spring.task.scheduling.enabled=false"
+})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class SubscriptionServiceTest {
     @Autowired
